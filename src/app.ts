6bda2480e2517025342a425647e89f5b47bd14c3
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-07-18 23:40:28 -0500

    wrapped up the ng-book 2 tutorial

--- src/app.ts
+++ src/app.ts
@@ -2,23 +2,95 @@
 
 import {Component, NgFor, View, bootstrap} from 'angular2/angular2';
 
-@Component({
-    selector: 'my-app'
-})
-@View({
-    directives: [NgFor],
-    template: `
-    <ul>
-        <li *ng-for="#name of names"><h1>Hello {{name}}</h1></li>
-    </ul>
-`
-})
-class myAppComponent {
-    names: Array<string>;
+class Article {
+    title: string;
+    link: string;
+    votes: number;
 
-    constructor() {
-        this.names = ['Alice', 'Bob', 'Charlie', 'Dave'];
+    constructor (title, link) {
+        this.title = title;
+        this.link = link;
+        this.votes = 0;
+    }
+
+    voteUp () {
+        this.votes += 1;
+        return false;
+    }
+
+    voteDown () {
+        this.votes -= 1;
+        return false;
+    }
+
+    domain () {
+        var link = this.link.split('//')[1];
+        return link.split('/')[0];
     }
 }
 
-bootstrap(myAppComponent);
+@Component({
+    selector: 'reddit-article',
+    properties: ['article']
+})
+@View({
+    template: `
+    <article>
+        <div class="votes">{{article.votes}}</div>
+        <div class="main">
+            <h2>
+                <a href="{{article.link}}">{{article.title}}</a>
+                <span>({{article.domain()}})</span>
+            </h2>
+            <ul>
+                <li><a href (click)="article.voteUp()">upvote</a></li>
+                <li><a href (click)="article.voteDown()">downvote</a></li>
+            </ul>
+        </div>
+    </article>
+    `
+})
+class RedditArticle {
+    article: Article;
+}
+
+@Component({
+    selector: 'reddit'
+})
+@View({
+    template: `
+    <section class="new-link">
+        <div class="control-group">
+            <div><label for="title">Title:</label></div>
+            <div><input name="title" #newtitle></div>
+        </div>
+        <div class="control-group">
+            <div><label for="link">Link:</label></div>
+            <div><input name="link" #newlink></div>
+        </div>
+
+        <button (click)="addArticle(newtitle, newlink)">Submit Link</button>
+    </section>
+
+    <reddit-article *ng-for="#article of articles" [article]="article"></reddit-article>
+    `,
+    directives: [RedditArticle, NgFor]
+})
+class RedditApp {
+    articles: Array<Article>;
+
+    constructor () {
+        this.articles = [
+            new Article('Angular 2', 'http://angular.io'),
+            new Article('Google', 'http://google.com')
+        ];
+    }
+
+    addArticle (title, link) {
+        this.articles.push(new Article(title.value, link.value));
+        title.value = '';
+        link.value = '';
+    }
+}
+
+bootstrap(RedditApp);
